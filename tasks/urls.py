53ecf1937eeddd1217fa1820c8e571fce9542621
Maksym Subchak <donjkee2@gmail.com>
2025-07-27 22:15:13 +0300

    feat: Add task create/update/detail views and templates
    Created task_form.html and task_detail.html templates. Added TaskCreateView, TaskUpdateView, and TaskDetailView to views. Integrated task detail and form views into dashboard.

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,12 +1,22 @@
 from django.urls import path
 
-from tasks.views import index, TaskListView, WorkerListView, WorkerDetailView
+from tasks.views import (
+    index, TaskListView,
+    WorkerListView,
+    WorkerDetailView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDetailView,
+)
 
 app_name = "taxi"
 
 urlpatterns = [
     path("", index, name="index"),
     path("dashboard/", TaskListView.as_view(), name="task-list"),
+    path("dashboard/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
+    path("dashboard/create/", TaskCreateView.as_view(), name="task-create"),
+    path("dashboard/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
     path("workers/", WorkerListView.as_view(), name="worker-list"),
     path("worker/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 from tasks.models import Worker, Task
 
 
-@login_required
+#@login_required
 def index(request: HttpRequest) -> HttpResponse:
     num_worker = Worker.objects.all().count()
     context = {
@@ -17,6 +18,30 @@ def index(request: HttpRequest) -> HttpResponse:
 
 class TaskListView(ListView):
     model = Task
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["num_workers"] = Worker.objects.count()
+        context["num_tasks"] = Task.objects.count()
+        context["num_in_progress"] = Task.objects.filter(is_complete=False).count()
+        context["num_done"] = Task.objects.filter(is_complete=True).count()
+        return context
+
+
+class TaskCreateView(CreateView):
+    model = Task
+    success_url = reverse_lazy("tasks:task_list")
+    fields = "__all__"
+
+
+class TaskDetailView(DetailView):
+    model = Task
+
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    success_url = reverse_lazy("tasks:task_list")
+    fields = "__all__"
 
 
 class WorkerListView(ListView):
