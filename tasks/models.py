742dca70f147fbec15e82299b033a306637bc9a0
Maksym Subchak <donjkee2@gmail.com>
2025-07-27 18:01:06 +0300

    feat: Add models TaskType with str, Task, Worker, and Position

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,41 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    class Priority(models.TextChoices):
+        URGENT = "urgent", "Urgent"
+        HIGH = "high", "High"
+        MEDIUM = "medium", "Medium"
+        LOW = "low", "Low"
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    is_complete = models.BooleanField(default=False)
+    priority = models.CharField(
+        max_length=10,
+        choices=Priority.choices,
+        default=Priority.MEDIUM,
+    )
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField("Worker", blank=True)
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey("Position", on_delete=models.SET_NULL, null=True)
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
